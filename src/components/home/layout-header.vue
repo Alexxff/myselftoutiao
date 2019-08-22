b9e2c76e07e4027692171ec1643c0434d227745a
xff <1065372633@qq.com>
2019-08-22 20:01:39 +0800

    主页模块-头部结构及下拉菜单增添样式

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -27,6 +27,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.layout-header{
+padding:12px 0;
+.left-header{
+  display:flex;
+  align-items: center;
+  i{
+    font-size: 20px;
+    margin-right:4px;
+  }
+}
+.layout-right{
+  display: flex;
+  align-items: center;
+  img{
+    width: 40px;
+    height: 40px;
+    border-radius:50%;
+    margin-right:10px;
+  }
+}
+}
 </style>
