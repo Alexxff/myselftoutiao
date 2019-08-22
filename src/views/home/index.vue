85b1f53e262d7f4c7dbd689eb76fec6842ce0ffe
xff <1065372633@qq.com>
2019-08-22 23:04:41 +0800

    主页模块-左侧导航配置路由

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,7 +8,9 @@
       <el-header>
         <layout-header></layout-header>
       </el-header>
-      <el-main>中间区域</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
