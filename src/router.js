6243a18734efb3e044d6ed9ab0035e8b1b2ddf9d
xufeifei <1065372633@qq.com>
2019-08-24 17:05:24 +0800

    评论列表-新建页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -6,31 +6,35 @@ import Main from './views/home/main'
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home,
-    children: [{
-      path: '',
-      component: Main
-    }]
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+      children: [{
+        path: '',
+        component: Main
+      }] },
+    {
+      path: 'home/comment',
+      component: () => import('./views/comment')
+    }
   ]
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+  // }
 })
