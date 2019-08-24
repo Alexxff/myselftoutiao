bf3f5fb3eed752acdc8f77762bd566c1c2ae4cf6
Your Name <you@example.com>
2019-08-24 17:36:32 +0800

    评论列表-页面结构-table的使用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,25 @@
 <template>
-<div>评论列表</div>
+<el-card>
+<bread-crumb slot='header'>
+<template slot=‘title>评论列表</template>
+</bread-crumb>
+<el-table>
+    <el-table-column width='500' label='标题'></el-table-column>
+    <el-table-column label="评论状态"></el-table-column>
+    <el-table-column label="总评论数"></el-table-column>
+    <el-table-column label="粉丝评论数"></el-table-column>
+    <el-table-column lable="操作"></el-table-column>
+</el-table>
+</el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
